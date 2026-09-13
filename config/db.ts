6bf56195057a61cf8d15405e0e5bacb9ccea823f
Jahidul Islam <jahidul.app@gmail.com>
2026-09-13 18:51:19 +0600

    chore: update package

--- config/db.ts
+++ config/db.ts
@@ -1,13 +1,20 @@
 import { PrismaClient } from "@/lib/generated/prisma";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 
-const getPrisma = () => new PrismaClient();
+const adapter = new PrismaMariaDb({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  connectionLimit: 5,
+});
+
+const getPrisma = () => new PrismaClient({ adapter });
 
 const globalForUserDBPrismaClient = global as unknown as {
-    db: ReturnType<typeof getPrisma>;
+  db: ReturnType<typeof getPrisma>;
 };
 
-export const db =
-    globalForUserDBPrismaClient.db || getPrisma();
+export const db = globalForUserDBPrismaClient.db || getPrisma();
 
-if (process.env.NODE_ENV !== "production")
-    globalForUserDBPrismaClient.db = db;
+if (process.env.NODE_ENV !== "production") globalForUserDBPrismaClient.db = db;
